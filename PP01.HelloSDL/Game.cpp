3b218393d08ce49c954edb24666e1559d6903e61
20171183 <20171183@vision.hoseo.edu>
2018-11-23 11:35:38 +0900

    PP05.AnimatingSprite

--- PP01.HelloSDL/Game.cpp
+++ PP01.HelloSDL/Game.cpp
@@ -18,11 +18,17 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 
 		m_bRunning = true;
 
-		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
 
-		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
 	}
 	else {
 		return false;
@@ -32,10 +38,6 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
 	SDL_RenderClear(m_pRenderer);
 	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
@@ -63,4 +65,9 @@ void Game::handleEvents()
 			break;
 		}
 	}
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
